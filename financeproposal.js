b63eb012b9e0ff4272a925c2590236f176837b20
veljko.ordic <veljko.ordic@gmail.com>
2025-04-23 17:11:50 +0200

    feat(finance) proposal

---
+++ financeproposal.js
@@ -0,0 +1,23 @@
+const Scraper = require('./lib/Scraper');
+
+// Define plugins configuration
+const plugins = {
+    'logger': {},
+    'finance-proposal': {closeAfterFind: true}
+};
+
+// Define storage configuration
+const storage = {
+    ConsoleStorage: { },
+    FileStorage: {  logDir:'data/fp',filenameFormat: '{hash}.json'}
+};
+
+// Create Scraper instance
+const scraper = new Scraper(plugins, storage);
+
+// Run the scraping
+const url = process.argv[2];
+const metadata = {};
+if (process.argv[3]) metadata.dealer_id = process.argv[3];
+if (process.argv[4]) metadata.car_id = process.argv[4];
+scraper.scrape(url, metadata);
