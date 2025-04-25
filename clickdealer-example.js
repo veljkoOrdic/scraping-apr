a3a9211e036f498d56c92892db3f11c5d742bde6
Zivorad Antonijevic <zivorad.antonijevic@beamusup.com>
2025-04-25 16:19:23 +0200

    feat(plugin) click dealer plugin

---
+++ clickdealer-example.js
@@ -0,0 +1,23 @@
+const Scraper = require('./lib/Scraper');
+
+// Define plugins configuration
+const plugins = {
+    'logger': {},
+    'click-dealer': {closeAfterFind: true}
+};
+
+// Define listeners configuration
+const listeners = {
+    ConsoleLog: { },
+    FileStorage: { filenameFormat: '{hash}.json'}
+};
+
+// Create Scraper instance
+const scraper = new Scraper(plugins, listeners);
+
+// Run the scraping
+const url = process.argv[2];
+const metadata = {};
+if (process.argv[3]) metadata.dealer_id = process.argv[3];
+if (process.argv[4]) metadata.car_id = process.argv[4];
+scraper.scrape(url, metadata);
